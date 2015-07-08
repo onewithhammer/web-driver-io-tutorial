2efaf37c166f0c242c625556b6aef0f3fc6a5c86
Tony Keith <tkeith@montrosetravel.com>
2015-07-08 16:03:03 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Web Driver IO Tutorial
+
+This is the README for the Web Driver IO Tutorial.
---
+++ Tutorial1.js
@@ -0,0 +1,45 @@
+// Tutorial1.js
+//
+// This is a simple test script that does the following:
+//  open a website
+//  validate title
+//
+
+// required libraries
+var webdriverio = require('webdriverio'),
+  should = require('should');
+
+// a test script block or suite
+describe('Title Test for Web Driver IO - Tutorial Test Page Website', function() {
+
+  // set timeout to 10 seconds
+	this.timeout(10000);
+  var driver = {};
+
+  // hook to run before tests
+  before( function (done) {
+    // load the driver for browser
+    driver = webdriverio.remote({ desiredCapabilities: {browserName: 'firefox'} });
+    driver.init(done);
+  });
+
+  // a test spec - "specification"
+  it('should be load correct page and title', function () {
+    // load page, then call function()
+    return driver
+      //.url('http://www.tlkeith.com/WebDriverIOTutorialTest.html')
+      .url('file:///Users/tkeith/Testing/Tutorial/HTML/WebDriverIOTutorialTest.html')
+      // get title, then pass title to function()
+      .getTitle().then( function (title) {
+        // verify title
+        (title).should.be.equal("Web Driver IO - Tutorial Test Page");
+        // uncomment for console debug
+        // console.log('Current Page Title: ' + title);
+      });
+  });
+
+  // a "hook" to run after all tests in this block
+	after(function(done) {
+    driver.end(done);
+  });
+});
