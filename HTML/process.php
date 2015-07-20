01fac26de96ae39ed93e02ddb742b24e527fc09e
Tony Keith <tkeith@montrosetravel.com>
2015-07-19 22:05:11 -0400

    Fixed typo

--- HTML/process.php
+++ HTML/process.php
@@ -1,14 +1,12 @@
 <html>
-<title>Search Sesults Page</title>
+<title>Search Results Page</title>
 <body>
 <div id="search-results">
 <?php
 $fname = $_POST['fname'];
 $lname = $_POST['lname']; 
 
-echo "<center><h1>";
-echo "Welcome " . $fname . " " . $lname . ".</h1><br>";
-
+echo "<center><h1>Welcome " . $fname . " " . $lname . ".</h1><br>";
 echo "<h2>Thank you for submitting your information!</h2></center>";
 
 ?>
