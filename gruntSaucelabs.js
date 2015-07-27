4e2c732701565a2b601527a26cf5e7450e3ed18d
Tony Keith <tkeith@montrosetravel.com>
2015-07-27 12:17:01 -0400

    Initial commit

---
+++ gruntSaucelabs.js
@@ -0,0 +1,55 @@
+// gruntSaucelabs.js
+//
+// This is a simple test script that does the following:
+//  open a website using cloud based test site (grunt and saucelabs)
+//  validate title
+//
+
+// required libraries
+var webdriverio = require('webdriverio'),
+  should = require('should');
+
+// a test script block or suite
+describe('Grunt Saucelabs Test for Web Driver IO - Tutorial Test Page Website', function() {
+
+  // set timeout to 10 seconds
+	this.timeout(10000);
+  var driver = {};
+
+  // hook to run before tests
+  before( function (done) {
+    // check for global browser
+    if(typeof browser === "undefined") {
+      // load the driver for browser
+      driver = webdriverio.remote({ desiredCapabilities: {browserName: 'firefox'} });
+      driver.init(done);
+    } else {
+      // grunt will load the browser driver
+      driver = browser;
+      done();
+    }
+  });
+
+  // a test spec - "specification"
+  it('should be load correct page and title', function () {
+    // load page, then call function()
+    return driver
+      .url('http://www.tlkeith.com/WebDriverIOTutorialTest.html')
+      // get title, then pass title to function()
+      .getTitle().then( function (title) {
+        // verify title
+        (title).should.be.equal("Web Driver IO - Tutorial Test Page");
+        // uncomment for console debug
+        // console.log('Current Page Title: ' + title);
+      });
+  });
+
+  // a "hook" to run after all tests in this block
+	after(function(done) {
+    if(typeof browser === "undefined") {
+      driver.end(done);
+    } else {
+      done();
+    }
+  });
+});
