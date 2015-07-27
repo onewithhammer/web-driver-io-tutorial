c3c0dedaf63e7f83c7ee36bcc672fd70ee8d18ed
Tony Keith <tkeith@montrosetravel.com>
2015-07-27 11:59:39 -0400

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    webdriver: {
+      options: {
+          host: 'ondemand.saucelabs.com',
+          port: 80,
+          user: process.env.SAUCE_USERNAME,
+          key: process.env.SAUCE_ACCESS_KEY,
+          tags: ['saucelabs'],
+          name: 'This is an example test script using grunt-driver and saucelabs'
+      },
+      ieexample: {
+        tests: ['tutorial2.js'],
+        options: {
+          desiredCapabilities: {
+            browserName: 'internet explorer',
+            version: '10.0',
+            platform: 'Windows 7'
+          }
+        }
+      },
+      chromeexample: {
+        tests: ['tutorial2.js'],
+        options: {
+          desiredCapabilities: {
+            browserName: 'chrome',
+            version: '42.0',
+            platform: 'Windows 8'
+          }
+        }
+      },
+      safariexample: {
+        tests: ['tutorial2.js'],
+        options: {
+          desiredCapabilities: {
+            browserName: 'safari',
+            version: '8.0',
+            platform: 'OS X 10.10'
+          }
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-webdriver');
+};
