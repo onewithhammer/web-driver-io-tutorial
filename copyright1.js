71f9fffce4e8458c8b1cb4a27371b86e0a2281e2
Tony Keith <tkeith@montrosetravel.com>
2015-08-04 17:04:46 -0400

    Fixed driver.end issue

--- copyright1.js
+++ copyright1.js
@@ -66,6 +66,10 @@ describe('Copyright Test for Web Driver IO - Tutorial Test Page Website', functi
 
   // a "hook" to run after all tests in this block
 	after(function(done) {
-    driver.end(done);
+    if(typeof browser === "undefined") {
+      driver.end(done);
+    } else {
+      done();
+    }
   });
 });
