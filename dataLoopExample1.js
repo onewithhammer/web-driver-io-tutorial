00a0e8d3fb7746862ca152d587c295233917b7ba
Tony Keith <tkeith@montrosetravel.com>
2015-08-27 00:58:59 -0400

    Updated with latest file list

--- dataLoopExample1.js
+++ dataLoopExample1.js
@@ -1,13 +1,13 @@
 // dataLoopExample1.js
 //
+// The example demos how to use an array of data and loop 
+//  through the data to verify URL/TEXT and the page exists.
+//
 // This is a simple test script that does the following:
 //  open a website
 //  validate title
 //  validates page with many links
-//
-//    each link is validated and the page is loaded
-
-// The example demos how to use an array of data and loop through the data to verify URL/TEXT and the page exists
+//  each link is validated and the link is loaded
 
 // required libraries
 var webdriverio = require('webdriverio'),
@@ -33,7 +33,13 @@ var linkArray = [
 {"link" : "https://github.com/onewithhammer/web-driver-io-tutorial/blob/master/cssValidation1.js", "name" : "cssValidation1.js"},
 {"link" : "https://github.com/onewithhammer/web-driver-io-tutorial/blob/master/saucelabs.js", "name" : "saucelabs.js"},
 {"link" : "https://github.com/onewithhammer/web-driver-io-tutorial/blob/master/gruntSaucelabs.js", "name" : "gruntSaucelabs.js"},
-{"link" : "https://github.com/onewithhammer/web-driver-io-tutorial/blob/master/Gruntfile.js", "name" : "Gruntfile.js"}
+{"link" : "https://github.com/onewithhammer/web-driver-io-tutorial/blob/master/Gruntfile.js", "name" : "Gruntfile.js"},
+{"link" : "https://github.com/onewithhammer/web-driver-io-tutorial/blob/master/Gruntfile-dataLoopExample2.js", "name" : "Gruntfile-dataLoopExample2.js"},
+{"link" : "https://github.com/onewithhammer/web-driver-io-tutorial/blob/master/Gruntfile-gruntSaucelabs.js", "name" : "Gruntfile-gruntSaucelabs.js"},
+{"link" : "https://github.com/onewithhammer/web-driver-io-tutorial/blob/master/wdio.conf-saucelabs-dataLoopExample2.js", "name" : "wdio.conf-saucelabs-dataLoopExample2.js"},
+{"link" : "https://github.com/onewithhammer/web-driver-io-tutorial/blob/master/wdio.conf-dataLoopExample2.js", "name" : "wdio.conf-dataLoopExample2.js"},
+{"link" : "https://github.com/onewithhammer/web-driver-io-tutorial/blob/master/restAPIExample1.js", "name" : "restAPIExample1.js"},
+{"link" : "https://github.com/onewithhammer/web-driver-io-tutorial/blob/master/dbDataLoopExample.js", "name" : "dbDataLoopExample.js"}
 ];
   // set timeout to 180 seconds since there are so many links to check
 	this.timeout(180000);
