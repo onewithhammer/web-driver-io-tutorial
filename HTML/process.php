255fe2f686cc722a694a9ca94f3c704a9e3101b2
Tony Keith <tkeith@montrosetravel.com>
2015-08-30 22:45:34 -0400

    Added order number

--- HTML/process.php
+++ HTML/process.php
@@ -1,5 +1,5 @@
 <html>
-<title>Search Results Page</title>
+<title>Web Driver IO - Tutorial Search Results Page</title>
 <body>
 <div id="search-results">
 <?php
@@ -7,7 +7,9 @@ $fname = $_POST['fname'];
 $lname = $_POST['lname']; 
 
 echo "<center><h1>Welcome " . $fname . " " . $lname . ".</h1><br>";
-echo "<h2>Thank you for submitting your information!</h2></center>";
+echo "<h2>Thank you for submitting your information!</h2><br>";
+
+echo "<h3>Your order number: <div id='orderNum'>" . rand() . "</div></h3></center>";
 
 ?>
 </div>
