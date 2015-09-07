c0bc3ebf45a722d87610dbedab3c20921fc826a2
Tony Keith <tkeith@montrosetravel.com>
2015-09-06 21:02:00 -0400

    Updated to use wdio

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,51 +6,16 @@
 //  $ grunt webdriver
 
 module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		webdriver: {
+			test: {
+				configFile: 'wdio.conf-gruntfile.js'
+			}
+		}
+	});
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    webdriver: {
-      options: {
-          host: (process.env.BUILD_NUMBER) ? 'localhost':'ondemand.saucelabs.com',
-          port: (process.env.BUILD_NUMBER) ? 4445:80,
-          user: process.env.SAUCE_USERNAME,
-          key: process.env.SAUCE_ACCESS_KEY,
-          tags: ['saucelabs'],
-          name: 'This is an example test script using grunt-driver and saucelabs'
-      },
-      ieexample: {
-        tests: ['gruntSaucelabs.js', 'tutorial1.js', 'copyright1.js', 'cssValidation1.js', 'dataLoopExample2.js', 'formFieldValidation.js', 'formFillSubmit1.js', 'linkTextURL1.js', 'linkTextURL2.js'],
-        options: {
-          desiredCapabilities: {
-            browserName: 'internet explorer',
-            version: '10.0',
-            platform: 'Windows 7'
-          }
-        }
-      },
-      chromeexample: {
-        tests: ['gruntSaucelabs.js', 'tutorial1.js', 'copyright1.js', 'cssValidation1.js', 'dataLoopExample2.js', 'formFieldValidation.js', 'formFillSubmit1.js', 'linkTextURL1.js', 'linkTextURL2.js'],
-        options: {
-          desiredCapabilities: {
-            browserName: 'chrome',
-            version: '42.0',
-            platform: 'Windows 8'
-          }
-        }
-      },
-      safariexample: {
-        tests: ['gruntSaucelabs.js', 'tutorial1.js', 'copyright1.js', 'cssValidation1.js', 'dataLoopExample2.js', 'formFieldValidation.js', 'formFillSubmit1.js', 'linkTextURL1.js', 'linkTextURL2.js'],
-        options: {
-          desiredCapabilities: {
-            browserName: 'safari',
-            version: '8.0',
-            platform: 'OS X 10.10'
-          }
-        }
-      }
-    }
-  });
-
-  grunt.loadNpmTasks('grunt-webdriver');
+grunt.loadNpmTasks('grunt-webdriver');
+// Default task(s).
+grunt.registerTask('default', ['webdriver']);
 };
