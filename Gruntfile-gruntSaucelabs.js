277d27f432efc5ddd6aceb861395298c60a75d37
Tony Keith <tkeith@montrosetravel.com>
2015-09-06 21:01:44 -0400

    Updated to use wdio

--- Gruntfile-gruntSaucelabs.js
+++ Gruntfile-gruntSaucelabs.js
@@ -4,51 +4,16 @@
 //  $ grunt --gruntfile Gruntfile-gruntSaucelabs.js webdriver
 
 module.exports = function(grunt) {
-
-  // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     webdriver: {
-      options: {
-          host: (process.env.BUILD_NUMBER) ? 'localhost':'ondemand.saucelabs.com',
-          port: (process.env.BUILD_NUMBER) ? 4445:80,
-          user: process.env.SAUCE_USERNAME,
-          key: process.env.SAUCE_ACCESS_KEY,
-          tags: ['saucelabs'],
-          name: 'This is an example test script using grunt-driver and saucelabs'
-      },
-      ieexample: {
-        tests: ['gruntSaucelabs.js'],
-        options: {
-          desiredCapabilities: {
-            browserName: 'internet explorer',
-            version: '10.0',
-            platform: 'Windows 7'
-          }
-        }
-      },
-      chromeexample: {
-        tests: ['gruntSaucelabs.js'],
-        options: {
-          desiredCapabilities: {
-            browserName: 'chrome',
-            version: '42.0',
-            platform: 'Windows 8'
-          }
-        }
-      },
-      safariexample: {
-        tests: ['gruntSaucelabs.js'],
-        options: {
-          desiredCapabilities: {
-            browserName: 'safari',
-            version: '8.0',
-            platform: 'OS X 10.10'
-          }
-        }
+      test: {
+        configFile: 'wdio.conf-gruntSaucelabs.js'
       }
     }
   });
 
-  grunt.loadNpmTasks('grunt-webdriver');
+grunt.loadNpmTasks('grunt-webdriver');
+// Default task(s).
+grunt.registerTask('default', ['webdriver']);
 };
