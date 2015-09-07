133532f5fa9b033286a43d3aa714adc10cfbe487
Tony Keith <tkeith@montrosetravel.com>
2015-09-06 22:04:25 -0400

    trying to find build issue

--- wdio.conf-gruntfile.js
+++ wdio.conf-gruntfile.js
@@ -39,7 +39,7 @@ exports.config = {
         //'cssValidation1.js',
         //'dataLoopExample2.js',
         //'formFieldValidation.js',
-        //'formFillSubmit1.js',
+        'formFillSubmit1.js',
         'linkTextURL1.js',
         'linkTextURL2.js',
         'carouselExample1.js',
