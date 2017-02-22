06343fdf7eb66ae961d477922ca38925a42b03c8
Tony Keith <tkeith@montrosetravel.com>
2017-02-21 22:18:59 -0500

    Testing

--- saucelabs.js
+++ saucelabs.js
@@ -15,26 +15,27 @@ var webdriverio = require('webdriverio'),
 describe('Saucelabs Test for Web Driver IO - Tutorial Test Page Website', function() {
 
   // set timeout to 10 seconds
-	this.timeout(10000);
+	this.timeout(9999999);
   var driver = {};
 
   // hook to run before tests
-  before( function (done) {
+  before( function () {
     // load the driver for browser
     driver = webdriverio.remote({
       desiredCapabilities: {
-            browserName: 'internet explorer',
-            version: '10.0',
-            platform: 'Windows 7',
+            browserName: 'firefox',
+            version: '50.0',
+            platform: 'Windows 8',
             tags: ['saucelabs'],
-            name: 'This is an example test script using saucelabs'
+            name: 'This is an example test script using saucelabs w Mocha as runner',
         },
         host: 'ondemand.saucelabs.com',
         port: 80,
         user: process.env.SAUCE_USERNAME,
         key: process.env.SAUCE_ACCESS_KEY,
-        logLevel: 'silent'})
-      .init(done);
+        updateJob: true,
+        logLevel: 'verbose'})
+      .init();
   });
 
   // a test spec - "specification"
@@ -52,7 +53,7 @@ describe('Saucelabs Test for Web Driver IO - Tutorial Test Page Website', functi
   });
 
   // a "hook" to run after all tests in this block
-	after(function(done) {
-    driver.end(done);
+	after(function() {
+    driver.end();
   });
 });
